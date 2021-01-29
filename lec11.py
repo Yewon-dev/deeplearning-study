1cb015579212d33e2258280fbd10a4d13ee3ede0
KimYewon <56240088+Yewon-dev@users.noreply.github.com>
2021-01-29 15:52:06 +0900

    csv example

---
+++ lec11.py
@@ -0,0 +1,9 @@
+import csv, codecs
+
+filename = "test.csv"
+file = codecs.open(filename,"r","euc_kr")
+
+reader = csv.reader(file, delimiter=",")
+
+for cells in reader:
+    print(cells[1],cells[2])
